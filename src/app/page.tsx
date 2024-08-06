0ab9151063237d71e71e4bf5c40cca4c0973ba37
natansantoz <ntsdo@aluno.ifnmg.edu.br>
2024-08-06 11:36:30 -0300

    deleta imagens da pasta public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,14 @@ const HomePage = () => {
   return (
     <div className="flex flex-col items-center w-full">
       <header className="py-4 w-full flex justify-center"> 
-        {/* <h1 className="text-3xl font-bold text-gray-800 text-center">RTCCIF</h1> */}
         <Image alt="logo" src={graduation_cap_image} height={200} width={200} />
       </header>
-      <div className="relative w-full max-w-4xl mx-4 md:mx-0 mt-4"> {/* Adicionar margem superior para ajustar o espaço */}
+      <div className="relative w-full max-w-4xl mx-4 md:mx-0 mt-4">
         <div className="absolute left-4 top-1/2 -translate-y-1/2 flex items-center text-gray-500">
           <i className="pi pi-search" style={{ fontSize: "1.25rem" }}></i>
         </div>
         <input
-          className="flex h-10 border border-input px-3 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md bg-background pl-12 pr-4 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-primary"
+          className="flex h-10 border-2 border-gray-300 px-3 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md bg-background pl-12 pr-4 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-primary"
           placeholder="Pesquisar monografias..."
           type="search"
         />
