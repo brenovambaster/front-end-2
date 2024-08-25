b085142d2ee9ae4d14d59b9ef9cc0fa6a52e210e
natansantoz <ntsdo@aluno.ifnmg.edu.br>
2024-08-25 15:08:58 -0300

    :sparkles: feat: nova pagina de perfil.

--- src/app/(rtcc)/perfil/page.tsx
+++ src/app/(rtcc)/perfil/page.tsx
@@ -1,212 +1,149 @@
 'use client';
-
-import React, { useState } from 'react';
-import { TabView, TabPanel } from 'primereact/tabview';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'tailwindcss/tailwind.css';
+import Image from 'next/image';
+import { Badge } from 'primereact/badge';
 import { Button } from 'primereact/button';
-import { IoArrowForward, IoHeart, IoStar } from 'react-icons/io5';
+import { Card } from 'primereact/card';
+import { useState } from 'react';
 
-const UserProfile = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
+const tccs = [
+    { id: 1, title: "Inteligência Artificial na Medicina", description: "Lucio fernandes dutra santos", tags: ["IA", "Medicina", "Bioinformática", "Medicina", "Medicina", "Medicina", "Medicina", "Medicina"] },
+    { id: 2, title: "Blockchain em Sistemas de Votação", description: "Implementação de um sistema de votação seguro usando tecnologia blockchain.", tags: ["Blockchain", "Segurança"] },
+    { id: 3, title: "Realidade Aumentada na Educação", description: "Desenvolvimento de aplicativos educacionais com realidade aumentada.", tags: ["AR", "Educação"] },
+    { id: 4, title: "Análise de Sentimentos em Redes Sociais", description: "Uso de processamento de linguagem natural para analisar opiniões online.", tags: ["NLP", "Redes Sociais"] },
+    { id: 5, title: "IoT na Agricultura de Precisão", description: "Aplicação de Internet das Coisas para otimizar a produção agrícola.", tags: ["IoT", "Agricultura"] },
+    { id: 6, title: "Cibersegurança em Sistemas Críticos", description: "Estratégias de proteção para infraestruturas críticas contra ataques cibernéticos.", tags: ["Cibersegurança"] },
+    { id: 7, title: "Computação Quântica: Algoritmos e Aplicações", description: "Estudo de algoritmos quânticos e suas potenciais aplicações práticas.", tags: ["Computação Quântica"] },
+    { id: 8, title: "Veículos Autônomos e Ética", description: "Análise dos dilemas éticos na programação de carros autônomos.", tags: ["IA", "Ética"] },
+    { id: 9, title: "Big Data na Previsão do Tempo", description: "Uso de grandes volumes de dados para melhorar previsões meteorológicas.", tags: ["Big Data", "Meteorologia"] },
+    { id: 10, title: "Bioinformática e Sequenciamento de DNA", description: "Desenvolvimento de algoritmos para análise eficiente de sequências genéticas.", tags: ["Bioinformática", "Genética"] },
+]
+
+export default function Component() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 4; // Agora exibirá 5 TCCs por página
+    const totalPages = Math.ceil(tccs.length / itemsPerPage);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentTCCs = tccs.slice(startIndex, endIndex);
 
     return (
-        <div className="relative">
-            <div
-                className="h-56 bg-cover bg-center"
-                style={{
-                    backgroundImage: `url('cover.jpg')`,
-                    backgroundSize: '100% 100%',
-                }}
-            >
-            </div>
-
-            <div className="absolute top-36 left-1/2 transform -translate-x-1/2 w-11/12 flex flex-col items-center gap-8">
-
-                <div className="flex w-full gap-8">
-                    <div
-                        style={{ boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.25)' }}
-                        className="w-1/5 bg-white rounded-lg p-4 border border-2 border-gray-400"
-                    >
-                        <div className="text-center">
-                            <img
+        <div className="mx-auto p-4 text-gray-800 mt-4">
+            {/* primeiro container */}
+            <div className="flex flex-col lg:flex-row gap-8 ">
+                <div className="lg:w-1/3 rounded-lg p-16 pt-6 mt-2">
+                    <div className="flex flex-col items-center border border-gray-400 rounded-lg p-8" style={{ boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.25)' }}>
+                        <div className="flex justify-center w-full mb-4">
+                            <Image
+                                alt=""
                                 src="/profile.png"
-                                alt="User Profile"
-                                className="rounded-full mx-auto"
-                                width="200"
-                                height="200"
+                                width="260"
+                                height="260"
+                                className="rounded-full border-0"
+                                style={{ height: 'auto' }}
                             />
-                            <h2 className="mt-4 text-xl font-semibold">Cirilo Netflixo da Silva</h2>
-                            <p className="text-gray-600">Acadêmico</p>
                         </div>
-                        <div className="mt-6 grid gap-4">
-                            <Button
-                                type="button"
-                                className="group border border-red-500 text-red-500 font-semibold py-3 px-4 rounded-md hover:bg-red-500 hover:text-white transition duration-300 flex items-center justify-center space-x-2"
-                            >
-                                <IoHeart className="text-red-500 group-hover:text-white text-lg transition duration-300" />
-                                <span className="text-lg font-bold">32</span>
-                                <span>Curtidos</span>
-                            </Button>
-                            <Button
-                                type="button"
-                                className="group border border-yellow-300 text-yellow-300 font-semibold py-3 px-4 rounded-md hover:bg-yellow-300 hover:text-white transition duration-300 flex items-center justify-center space-x-2"
-                            >
-                                <IoStar className="text-yellow-300 group-hover:text-white text-lg transition duration-300" />
-                                <span className="text-lg font-bold">62</span>
-                                <span>Favoritos</span>
-                            </Button>
+                        <h1 className="text-2xl font-bold mb-1 text-center">Cirilo Netflixo da Silva</h1>
+                        <p className="text-gray-600 mb-4 text-center">Acadêmico</p>
+                        <Button
+                            icon="pi pi-pencil"
+                            label="Editar Conta"
+                            className="mb-4 w-48" // Linha alterada: Define a largura do botão como 48 unidades (pode ajustar conforme necessário)
+                            style={{
+                                backgroundColor: '#2b2d39',
+                                borderColor: '#2b2d39',
+                                borderWidth: '1px',
+                                borderStyle: 'solid'
+                            }}
+                        />
+                        <div className="flex flex-col items-center mt-6">
+                            <div className="flex items-center gap-4 mb-4">
+                                <span className="flex items-center">
+                                    <i className="pi pi-heart-fill text-red-500 mr-1"></i>
+                                    <span className="font-bold text-red-500">26</span>
+                                    <span className="text-gray-600 ml-1">Curtidos</span>
+                                </span>
+                                <span className="flex items-center">
+                                    <i className="pi pi-star-fill text-yellow-500 mr-1"></i>
+                                    <span className="font-bold text-yellow-500">62</span>
+                                    <span className="text-gray-600 ml-1">Favoritos</span>
+                                </span>
+                            </div>
                         </div>
-                    </div>
-
-                    <div
-                        className="w-3/6 bg-white rounded-lg p-6 border border-2 border-gray-400"
-                        style={{ boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.25)', height: '470px' }}
-                    >
-                        <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
-                            <TabPanel
-                                header={
-                                    <span
-                                        className={`text-base px-4 py-2 cursor-pointer ${activeIndex === 0
-                                            ? 'text-black border-b-2 border-black'
-                                            : 'text-black'
-                                            }`}
-                                    >
-                                        Detalhes da Conta
-                                    </span>
-                                }
-                            >
-                                <div className="grid grid-cols-1 gap-6">
-                                    <div>
-                                        <label className="block font-medium">Nome</label>
-                                        <input
-                                            type="text"
-                                            value="Cirilo Netflixo da Silva"
-                                            className="w-full mt-2 p-2 border rounded-lg"
-                                        />
-                                    </div>
-                                    <div>
-                                        <label className="block font-medium">E-mail</label>
-                                        <input
-                                            type="text"
-                                            value="mail@aluno.ifnmg.edu.br"
-                                            className="w-full mt-2 p-2 border rounded-lg"
-                                        />
-                                    </div>
-                                    <div>
-                                        <label className="block font-medium">Curso</label>
-                                        <input
-                                            type="text"
-                                            value="+1800-000"
-                                            className="w-full mt-2 p-2 border rounded-lg"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="mt-6 py-2 rounded-lg">
-                                    <Button
-                                        type="button"
-                                        label="Salvar Alterações"
-                                        className="w-full text-white font-semibold py-2 rounded-md hover:bg-gray-800 transition duration-300"
-                                        style={{ backgroundColor: '#2b2d39', borderColor: '#2b2d39', borderWidth: '1px', borderStyle: 'solid' }}
-                                    />
-                                </div>
-
-                            </TabPanel>
-
-                            <TabPanel
-                                header={
-                                    <span
-                                        className={`text-base px-4 py-2 cursor-pointer ${activeIndex === 1
-                                            ? 'text-black border-b-2 border-black'
-                                            : 'text-black'
-                                            }`}
-                                    >
-                                        Alterar Senha
-                                    </span>
-                                }
-                            >
-                                <div className="grid grid-cols-1 gap-6">
-                                    <div>
-                                        <label className="block font-medium">Senha Atual</label>
-                                        <input
-                                            type="password"
-                                            placeholder="Digite a senha atual"
-                                            className="w-full mt-2 p-2 border rounded-lg"
-                                        />
-                                    </div>
-                                    <div>
-                                        <label className="block font-medium">Nova Senha</label>
-                                        <input
-                                            type="password"
-                                            placeholder="Digite a nova senha"
-                                            className="w-full mt-2 p-2 border rounded-lg"
-                                        />
-                                    </div>
-                                    <div>
-                                        <label className="block font-medium">Confirmar Nova Senha</label>
-                                        <input
-                                            type="password"
-                                            placeholder="Confirme a nova senha"
-                                            className="w-full mt-2 p-2 border rounded-lg"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="mt-6 py-2 rounded-lg">
-                                    <Button
-                                        type="button"
-                                        label="Alterar Senha"
-                                        className="w-full text-white font-semibold py-2 rounded-md hover:bg-gray-800 transition duration-300"
-                                        style={{ backgroundColor: '#2b2d39', borderColor: '#2b2d39', borderWidth: '1px', borderStyle: 'solid' }}
-                                    />
-                                </div>
-                            </TabPanel>
-                        </TabView>
                     </div>
                 </div>
 
-                <div
-                    className="w-3/6 bg-white rounded-lg p-6 border border-2 border-gray-400"
-                    style={{ boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.25)', height: '470px' }}
-                >
-                    <TabView >
-                        <TabPanel
-                            header={
-                                <span
-                                    className={`text-base px-4 py-2 cursor-pointer ${activeIndex === 0
-                                        ? 'text-black border-b-2 border-black'
-                                        : 'text-black'
-                                        }`}
-                                >
-                                    Detalhes da Conta
-                                </span>
-                            }
-                        >
-                            {/* conteudo */}
-                        </TabPanel>
 
-                        <TabPanel
-                            header={
-                                <span
-                                    className={`text-base px-4 py-2 cursor-pointer ${activeIndex === 1
-                                        ? 'text-black border-b-2 border-black'
-                                        : 'text-black'
-                                        }`}
-                                >
-                                    Alterar Senha
-                                </span>
-                            }
-                        >
-                            {/* conteudo */}
+                {/* segundo container */}
+                <div className="lg:w-2/3 mr-8">
+                    <h3 className="text-md font-semibold mb-2">Favoritos</h3>
+                    <div className="grid gap-4"> {/* Aumentado o espaçamento entre os cards */}
+                        {currentTCCs.map((tcc) => (
+                            <Card
+                                key={tcc.id}
+                                title={<span style={{ fontSize: '16px', fontWeight: 'strong' }}>{tcc.title}</span>}
+                                className="text-xs font-light border border-gray-400"
+                            >
+                                <p className="text-sm font-medium text-gray-600 mb-1">
+                                    {tcc.description}
+                                </p>
+                                <div className="flex flex-wrap gap-0.5">
+                                    {tcc.tags.map((tag, index) => (
+                                        <Badge
+                                            key={index}
+                                            value={tag}
+                                            className="text-white text-3xs mr-0.5 mt-4"
+                                            style={{ backgroundColor: '#2b2d39' }}
+                                        />
+                                    ))}
+                                </div>
+                            </Card>
+                        ))}
+                    </div>
 
-                        </TabPanel>
-                    </TabView>
+                    {/* terceiro container */}
+                    <div className="flex gap-8 items-center mt-8 justify-center">
+                        <Button
+                            icon="pi pi-chevron-left"
+                            label=""
+                            className="p-button-outlined p-button-sm"
+                            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                            disabled={currentPage === 1}
+                            style={{
+                                backgroundColor: '#2b2d39',
+                                borderColor: '#2b2d39',
+                                borderWidth: '1px',
+                                borderStyle: 'solid',
+                                color: '#fff',
+                                fontSize: '0.875rem',
+                                padding: '0.5rem 1rem'
+                            }}
+                        />
+
+                        <span className="text-md font-medium text-gray-600">
+                            Página {currentPage} de {totalPages}
+                        </span>
+                        <Button
+                            icon="pi pi-chevron-right"
+                            label=""
+                            iconPos="right"
+                            className="p-button-outlined p-button-sm p"
+                            onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                            disabled={currentPage === totalPages}
+                            style={{
+                                backgroundColor: '#2b2d39',
+                                borderColor: '#2b2d39',
+                                borderWidth: '1px',
+                                borderStyle: 'solid',
+                                color: '#fff',
+                                fontSize: '0.875rem',
+                                padding: '0.5rem 1rem'
+                            }}
+                        />
+
+                    </div>
                 </div>
             </div>
-
         </div>
     );
-};
-
-export default UserProfile;
+}
