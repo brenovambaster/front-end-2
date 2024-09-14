b3047dfd437e3489de85d31ffdecff9f1dbfc553
natansantoz <ntsdo@aluno.ifnmg.edu.br>
2024-09-14 00:44:27 -0300

    feat: cria tela de trending topics

--- src/app/(rtcc)/trending-topics/Example.tsx
+++ src/app/(rtcc)/trending-topics/Example.tsx
@@ -8,8 +8,7 @@ interface ExampleProps {
     width: number;
     height: number;
     showControls?: boolean;
-    fontSizeSetter: (datum: WordData) => number;
-    words: WordData[];
+    keywordsText: string;
 }
 
 export interface WordData {
@@ -17,106 +16,119 @@ export interface WordData {
     value: number;
 }
 
-const colors = ['#20222c', '#474b5f', '#646985', '#ffbd59'];
+const colors = ['#20222c', '#474b5f', '#646985', '#cc9747'];
 
-function wordFreq(text: string): WordData[] {
-    const words: string[] = text.replace(/\./g, '').split(/\s+/);
-    const freqMap: Record<string, number> = {};
-
-    for (const w of words) {
-        if (!freqMap[w]) freqMap[w] = 0;
-        freqMap[w] += 1;
-    }
-    return Object.keys(freqMap).map((word) => ({ text: word, value: freqMap[word] }));
-}
-
-function getRotationDegree() {
-    const rand = Math.random();
-    const degree = rand > 0.5 ? 60 : -60;
-    return rand * degree;
-}
-
-// const words = wordFreq(totoAfricaLyrics);
-
-// Ajuste o intervalo de escala da fonte para aumentar o tamanho da fonte
-// const fontScale = scaleLog({
-//     domain: [Math.min(...words.map((w) => w.value)), Math.max(...words.map((w) => w.value))],
-//     range: [20, 150], // Aumente o valor máximo para um tamanho maior
-// });
-// const fontSizeSetter = (datum: WordData) => fontScale(datum.value);
-
-const fixedValueGenerator = () => 0.5;
 
 type SpiralType = 'archimedean' | 'rectangular';
 
-export default function Example({ width, height, showControls, fontSizeSetter, words }: ExampleProps) {
+export default function Example({ width, height, showControls, keywordsText }: ExampleProps) {
     const [spiralType, setSpiralType] = useState<SpiralType>('archimedean');
     const [withRotation, setWithRotation] = useState(false);
 
-    return (
-        <div className="container">
-            <div className="wordcloud">
-                {/* Botão para dar um alert nos tccs fetcheds
-            <button
-                onClick={() => {
-                    console.log(JSON.stringify(words));
-                }}
-            >ZZZZZZZZZZZZ</button> */}
-                <Wordcloud
-                    words={words}
-                    width={width}
-                    height={height}
-                    fontSize={fontSizeSetter}
-                    font={'Impact'}
-                    padding={2}
-                    spiral={spiralType}
-                    rotate={function () { return (~~(Math.random() * 6) - 3) * 30; }}
-                    random={fixedValueGenerator}
-                >
-                    {(cloudWords) =>
-                        cloudWords.map((w, i) => (
-                            <Text
-                                key={w.text}
-                                fill={colors[i % colors.length]}
-                                textAnchor={'middle'}
-                                transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
-                                fontSize={w.size}
-                                fontFamily={w.font}
-                            >
-                                {w.text}
-                            </Text>
-                        ))
-                    }
-                </Wordcloud>
-            </div>
-            <style jsx>{`
-        .container {
-          display: flex;
-          justify-content: center; /* Centraliza horizontalmente */
-          width: 100%;
+    function wordFreq(text: string): WordData[] {
+        // Alterar o split para usar '|' como separador
+        const words: string[] = text.split('|');
+        const freqMap: Record<string, number> = {};
+    
+        for (const w of words) {
+            if (!freqMap[w]) freqMap[w] = 0;
+            freqMap[w] += 1;
         }
+        return Object.keys(freqMap).map((word) => ({ text: word, value: freqMap[word] }));
+    }
+    
+
+    function getRotationDegree() {
+        const rand = Math.random();
+        const degree = rand > 0.5 ? 60 : -60;
+        return rand * degree;
+    }
+
+    const words = wordFreq(keywordsText);
+
+    const fontScale = scaleLog({
+        domain: [Math.min(...words.map((w) => w.value)), Math.max(...words.map((w) => w.value))],
+        range: [20, 50],
+    });
+    const fontSizeSetter = (datum: WordData) => fontScale(datum.value);
+
+    const fixedValueGenerator = () => 0.5;
+
+
+    return (
+        <div className="wordcloud">
+            <Wordcloud
+                words={words}
+                width={width}
+                height={height}
+                fontSize={fontSizeSetter}
+                font={'Impact'}
+                padding={2}
+                spiral={spiralType}
+                rotate={withRotation ? getRotationDegree : 0}
+                random={fixedValueGenerator}
+            >
+                {(cloudWords) =>
+                    cloudWords.map((w, i) => (
+                        <Text
+                            key={w.text}
+                            fill={colors[i % colors.length]}
+                            textAnchor={'middle'}
+                            transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
+                            fontSize={w.size}
+                            fontFamily={w.font}
+                        >
+                            {w.text}
+                        </Text>
+                    ))
+                }
+            </Wordcloud>
+            {showControls && (
+                <div>
+                    <label>
+                        Spiral type &nbsp;
+                        <select
+                            onChange={(e) => setSpiralType(e.target.value as SpiralType)}
+                            value={spiralType}
+                        >
+                            <option key={'archimedean'} value={'archimedean'}>
+                                archimedean
+                            </option>
+                            <option key={'rectangular'} value={'rectangular'}>
+                                rectangular
+                            </option>
+                        </select>
+                    </label>
+                    <label>
+                        With rotation &nbsp;
+                        <input
+                            type="checkbox"
+                            checked={withRotation}
+                            onChange={() => setWithRotation(!withRotation)}
+                        />
+                    </label>
+                    <br />
+                </div>
+            )}
+            <style jsx>{`
         .wordcloud {
           display: flex;
           flex-direction: column;
           user-select: none;
-          width: 100%;
-          max-width: ${width}px; /* Define a largura máxima */
-          height: ${height}px; /* Define a altura do componente */
         }
         .wordcloud svg {
-          width: 100%;
-          height: 100%;
-          margin: 0;
+          margin: 1rem 0;
           cursor: pointer;
         }
-        .controls {
-          margin-top: 1rem;
-        }
-        .controls label {
+
+        .wordcloud label {
           display: inline-flex;
           align-items: center;
           font-size: 14px;
           margin-right: 8px;
+        }
+        .wordcloud textarea {
+          min-height: 100px;
         }
       `}</style>
         </div>
--- src/app/(rtcc)/trending-topics/page.tsx
+++ src/app/(rtcc)/trending-topics/page.tsx
@@ -6,25 +6,7 @@ import { FaHeart, FaStar } from 'react-icons/fa';
 import { useState, useEffect } from 'react';
 import { AiOutlineLoading } from 'react-icons/ai';
 import { TCCService } from '@/service/tccService';
-import { scaleLog } from '@visx/scale';
-
-
-type Keyword = {
-    name: string;
-};
-
-type DataObject = {
-    id: string;
-    title: string;
-    keywords: Keyword[];
-};
-
-type WordData = {
-    text: string;
-    value: number;
-};
-
-
+import Link from 'next/link';
 
 
 export default function Component() {
@@ -33,15 +15,23 @@ export default function Component() {
     const [mostFavoritedTCCs, setMostFavoritedTCCs] = useState<any[]>([]);
     const [allTCCs, setAllTCCs] = useState<any[]>([]);
     const [fetchingTCCs, setFetchingTCCs] = useState(true);
+    const [keywordsText, setKeywordsText] = useState<string>('');
+
+    const handleCardClick = (tccId: string) => {
+        const url = `/tcc/${tccId}`;
+        window.open(url, '_blank');
+    };
 
     useEffect(() => {
-        setLoading(false);
 
         const getTCC = async () => {
             try {
                 const mostLikedTCCsResponse = await TCCService.getTCCs();
                 const mostFavoritedTCCsResponse = await TCCService.getTCCs();
                 const allTCCsResponse = await TCCService.getTCCs();
+
+                setKeywordsText(allTCCsResponse.map((tcc) => tcc.keywords.map((keyword) => keyword.name).join('|')).join('|'));
+                
                 setMostLikedTCCs(mostLikedTCCsResponse);
                 setMostFavoritedTCCs(mostFavoritedTCCsResponse);
                 setAllTCCs(allTCCsResponse);
@@ -50,50 +40,15 @@ export default function Component() {
                 console.error("Error fetching TCCs:", error);
             } finally {
                 setFetchingTCCs(false);
+                setLoading(false);
             }
         };
 
         getTCC();
     }, []);
 
-
-    function wordFreq(dataArray: DataObject[]): WordData[] {
-        const freqMap: Record<string, number> = {};
-
-        
-        for (const data of dataArray) {
-            
-            for (const keyword of data.keywords) {
-                const word = keyword.name.trim(); 
-
-                if (word) { 
-                    if (!freqMap[word]) freqMap[word] = 0;
-                    freqMap[word] += 1;
-                }
-            }
-        }
-
-        return Object.keys(freqMap).map((word) => ({ text: word, value: freqMap[word] }));
-    }
-
-
-    const words = wordFreq(mostLikedTCCs);
-
-    
-    const fontScale = scaleLog({
-        domain: [Math.min(...words.map((w) => w.value)), Math.max(...words.map((w) => w.value))],
-        range: [1, 50], 
-    });
-    const fontSizeSetter = (datum: WordData) => fontScale(datum.value);
-
     return (
         <div className="w-full px-4 py-8">
-
-            {/* <button
-                onClick={() => {
-                    alert(JSON.stringify(words));
-                }}
-            >ZZZZZZZZZZZZ</button> */}
             {loading && (
                 <div className="absolute top-0 left-0 w-full h-full bg-white flex flex-col justify-center items-center z-10">
                     <AiOutlineLoading className="text-black text-5xl animate-spin" />
@@ -101,7 +56,7 @@ export default function Component() {
                 </div>
             )}
             <div className={`mt-4 flex justify-center`}>
-                <Example width={800} height={600} fontSizeSetter={fontSizeSetter} words={words} />
+                <Example width={800} height={600} keywordsText={keywordsText} />
             </div>
 
             <div className="mb-4 mt-8 ml-8">
@@ -111,6 +66,7 @@ export default function Component() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 ml-8 mr-8">
+
                 <Card
                     title={
                         <span className="font-semibold text-lg flex items-center">
@@ -121,17 +77,23 @@ export default function Component() {
                     className="text-2xl border border-gray-300 shadow-none"
                 >
                     {mostLikedTCCs.sort((a, b) => b.likes - a.likes).map(tcc => (
-                        <div key={tcc.id} className="mb-2 p-4 bg-gray-100 rounded-lg border border-gray-400">
-                            <h3 className="font-semibold h-16 overflow-hidden break-words">{tcc.title}</h3>
-                            <p className="text-sm text-gray-600 mt-2">{tcc.author}</p>
-                            <Tag
-                                value={tcc.numLikes}
-                                icon={<FaHeart style={{ color: '#ff3040', marginRight: '4px' }} />}
-                                className="mt-2 bg-transparent border-none text-gray-800 text-sm"
-                            />
-                        </div>
+                        <Link href="" onClick={(e) => {
+                            e.preventDefault();
+                            handleCardClick(tcc.id);
+                        }}>
+                            <div key={tcc.id} className="mb-2 p-4 bg-gray-100 rounded-lg border border-gray-400">
+                                <h3 className="font-semibold h-16 overflow-hidden break-words">{tcc.title}</h3>
+                                <p className="text-sm text-gray-600 mt-2">{tcc.author}</p>
+                                <Tag
+                                    value={tcc.numLikes}
+                                    icon={<FaHeart style={{ color: '#ff3040', marginRight: '4px' }} />}
+                                    className="mt-2 bg-transparent border-none text-gray-800 text-sm"
+                                />
+                            </div>
+                        </Link>
                     ))}
                 </Card>
+
 
                 <Card
                     title={
@@ -143,17 +105,26 @@ export default function Component() {
                     className="text-2xl border border-gray-300 shadow-none"
                 >
                     {mostFavoritedTCCs.sort((a, b) => b.favorites - a.favorites).map(tcc => (
-                        <div key={tcc.id} className="mb-2 p-4 bg-gray-100 rounded-lg border border-gray-400">
-                            <h3 className="font-semibold h-16 overflow-hidden">{tcc.title}</h3>
-                            <p className="text-sm text-gray-600 mt-2">{tcc.author}</p>
-                            <Tag
-                                value={tcc.numFavorites}
-                                icon={<FaStar style={{ color: '#f59e0b', marginRight: '4px' }} />}
-                                className="mt-2 bg-transparent border-none text-gray-800 text-sm"
-                            />
-                        </div>
+                        <Link href="" onClick={(e) => {
+                            e.preventDefault();
+                            handleCardClick(tcc.id);
+                        }}>
+                            <div
+                                key={tcc.id}
+                                className="mb-2 p-4 bg-gray-100 rounded-lg border border-gray-400 cursor-pointer"
+                            >
+                                <h3 className="font-semibold h-16 overflow-hidden">{tcc.title}</h3>
+                                <p className="text-sm text-gray-600 mt-2">{tcc.author}</p>
+                                <Tag
+                                    value={tcc.numFavorites}
+                                    icon={<FaStar style={{ color: '#f59e0b', marginRight: '4px' }} />}
+                                    className="mt-2 bg-transparent border-none text-gray-800 text-sm"
+                                />
+                            </div>
+                        </Link>
                     ))}
                 </Card>
+
             </div>
         </div>
     );
